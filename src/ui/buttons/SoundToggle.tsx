df15283d8c5aa6fa406d3bf6d77ad6ba792e8c23
tarasua <taras.rutkovskyi@gmail.com>
2025-10-18 17:53:37 +0300

    updated img-path

--- src/ui/buttons/SoundToggle.tsx
+++ src/ui/buttons/SoundToggle.tsx
@@ -19,7 +19,7 @@ export class SoundToggle extends React.Component<Props, State> {
     return (
       <div className="absolute top-3 right-3 z-[1000] pointer-events-auto">
         <img
-          src={this.state.enabled ? "/sound-on.png" : "/sound-off.png"}
+          src={this.state.enabled ? "sound-on.png" : "sound-off.png"}
           width={48}
           height={48}
           className="cursor-pointer transition-transform hover:scale-110 active:scale-95"
